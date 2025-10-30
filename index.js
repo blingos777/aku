b896111c5f0518a20a7078f8bbe5c8e7fa70a49f
blingos777 <108165415+blingos777@users.noreply.github.com>
2025-10-30 18:18:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,15 @@ const initializeClients = async () => {
             continue;
         }
 
-        const client = new Client({
-            intents: [
-                Intents.FLAGS.GUILDS,
-                Intents.FLAGS.GUILD_MESSAGES,
-                Intents.FLAGS.GUILD_MEMBERS,
-                Intents.FLAGS.GUILD_PRESENCES,
-                Intents.FLAGS.DIRECT_MESSAGES
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
             ],
             partials: ['CHANNEL']
         });
